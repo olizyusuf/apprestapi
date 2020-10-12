7e8d884d8806c22b82fa8a43a60960024bb48d7d
olizyusuf <olizyusuf22@gmail.com>
2020-10-12 21:22:07 +0700

    edit nama routes dan menambahkan ke server

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const port = 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// panggil routes
+var routes = require('./routes');
+routes(app);
+
+
+
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 });
