f756325d9562de20c9d253a5f1d0b7967a0695a9
olizyusuf <olizyusuf22@gmail.com>
2020-10-12 21:40:15 +0700

    menambah fitur list data mahasiswa dengan id

--- controller.js
+++ controller.js
@@ -3,14 +3,14 @@
 var response = require('./res');
 var connection = require('./koneksi');
 
-exports.index = function(req, res){
+exports.index = function (req, res) {
     response.ok("Aplikasi REST API Berjalan!", res);
 };
 
 // menampilkan data mahasiswa
 
-exports.listAllMahasiswa = function(req,res) {
-    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields) {
+exports.listAllMahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
         if (error) {
             connection.log(error);
         } else {
@@ -18,3 +18,15 @@ exports.listAllMahasiswa = function(req,res) {
         }
     });
 };
+
+exports.listDataMahasiswa = function (req, res) {
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa=?', [id],
+        function (error, rows, fields) {
+            if (error) {
+                connection.log(error);
+            } else {
+                response.ok(rows, res);
+            }
+        });
+}
--- routes.js
+++ routes.js
@@ -8,4 +8,7 @@ module.exports = function(app) {
 
     app.route('/tampil')
         .get(jsonku.listAllMahasiswa);
+
+    app.route('/tampil/:id')
+        .get(jsonku.listDataMahasiswa);
 }
