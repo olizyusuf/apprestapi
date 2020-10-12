bfb85fcdf03a1c5c529116e414749d844c4cde72
olizyusuf <olizyusuf22@gmail.com>
2020-10-12 21:34:14 +0700

    berhasil menampilan data mahasiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,15 @@ var connection = require('./koneksi');
 exports.index = function(req, res){
     response.ok("Aplikasi REST API Berjalan!", res);
 };
+
+// menampilkan data mahasiswa
+
+exports.listAllMahasiswa = function(req,res) {
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields) {
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    });
+};
